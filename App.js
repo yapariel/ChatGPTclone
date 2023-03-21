3fe4b91bd974b45b5fa64a5d155a9f2dbc852703
John Ariel Yap <johnarielyap143@gmail.com>
2023-03-20 19:41:08 -0800

    Add files via upload

---
+++ App.js
@@ -0,0 +1,40 @@
+import React from "react";
+import "./App.css";
+import { Configuration, OpenAIApi } from "openai";
+import OptionSelection from "./components/OptionSelection";
+import Translation from "./components/Translation";
+import { arrayItems } from "./AIOptions";
+import { useState } from "react";
+
+function App() {
+  const configuration = new Configuration({
+    apiKey: "sk-dwdwadwadwadawdawdawdwadwadawdawdwa",
+  });
+  const openai = new OpenAIApi(configuration);
+  const [option, setOption] = useState({});
+  const [result, setResult] = useState("");
+  const [input, setInput] = useState("");
+  const selectOption = (option) => {
+    setOption(option);
+  };
+
+  const doStuff = async () => {
+    let object = { ...option, prompt: input };
+
+    const response = await openai.createCompletion(object);
+
+    setResult(response.data.choices[0].text);
+  };
+
+  return (
+    <div className="App">
+      {Object.values(option).length === 0 ? (
+        <OptionSelection arrayItems={arrayItems} selectOption={selectOption} />
+      ) : (
+        <Translation doStuff={doStuff} setInput={setInput} result={result} />
+      )}
+    </div>
+  );
+}
+
+export default App;
